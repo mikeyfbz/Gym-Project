9280c4442e1d7fbdcd732914891a335025d587fe
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-04-25 16:05:53 +0100

    database createdls

---
+++ db/gym.sql
@@ -0,0 +1,24 @@
+DROP TABLE bookings;
+DROP TABLE classes;
+DROP TABLE members;
+
+CREATE TABLE members (
+  id SERIAL8 PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  age INT
+);
+
+CREATE TABLE classes (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  type VARCHAR(255),
+  capacity INT,
+  time FLOAT
+);
+
+CREATE TABLE bookings (
+  id SERIAL8 PRIMARY KEY,
+  member_id INT8 REFERENCES members(id) ON DELETE CASCADE,
+  class_id INT8 REFERENCES classes(id) ON DELETE CASCADE
+);
