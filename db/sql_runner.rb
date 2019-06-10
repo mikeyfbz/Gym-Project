f00239658fa00bcf1b0eb6e9cc1ec93cf663f679
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-06-10 13:01:24 +0100

    db setup

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,11 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'gym', host: 'localhost' })
+      db = PG.connect({ dbname: 'dbrfhjq6hnpi9b',
+                         host: 'ec2-75-101-128-10.compute-1.amazonaws.com
+                         ',
+                        user: 'ldqmsjtdplxnox',
+                        password: '603e58fdb06438c8bbadbcd9555a1ae0d4e81a0b32b7b30d712c3d3a29af6abf' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
