aca1325b7c6ce92348c8ddc66a6e8d0cf6d48f82
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-06-10 12:56:30 +0100

    also reload removed

--- main_controller.rb
+++ main_controller.rb
@@ -4,7 +4,7 @@ require('pg')
 require_relative('./controllers/classes_controller.rb')
 require_relative('./controllers/members_controller.rb')
 require_relative('./controllers/bookings_controller.rb')
-also_reload('./models/*')
+# also_reload('./models/*')
 
 get '/' do
   erb(:index)
